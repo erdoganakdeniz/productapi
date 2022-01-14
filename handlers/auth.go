2f3cde77e8836339bcaebaf62b000e8eba965a22
erdoganakdeniz <erdo44@hotmail.de>
2022-01-14 14:28:01 +0300

    edited

--- handlers/auth.go
+++ handlers/auth.go
@@ -17,25 +17,25 @@ type AuthHandler struct {
 	UsersColl *mongo.Collection
 }
 
-func (a AuthHandler) Login(c *fiber.Ctx)  {
-	u:=new(models.LoginInputs)
-	if err:=c.BodyParser(u);err != nil {
+func (a AuthHandler) Login(c *fiber.Ctx) {
+	u := new(models.LoginInputs)
+	if err := c.BodyParser(u); err != nil {
 		c.Status(fiber.StatusBadRequest).Send(err)
 		return
 	}
-	user:=new(models.User)
-	filter:=bson.M{"email":u.Email}
-	err:=a.UsersColl.FindOne(c.Fasthttp,filter).Decode(user)
+	user := new(models.User)
+	filter := bson.M{"email": u.Email}
+	err := a.UsersColl.FindOne(c.Fasthttp, filter).Decode(user)
 	if err != nil {
-		c.Status(fiber.StatusUnauthorized).Send(fiber.Map{"message":"Invalid Credentials"})
+		c.Status(fiber.StatusUnauthorized).Send(fiber.Map{"message": "Invalid Credentials"})
 		return
 	}
-	isMatch:=utils.Password{Password: u.Password}.Compare(user.Password)
-	if !isMatch{
-		c.Status(fiber.StatusUnauthorized).Send(fiber.Map{"message":"Invalid Credentials"})
+	isMatch := utils.Password{Password: u.Password}.Compare(user.Password)
+	if !isMatch {
+		c.Status(fiber.StatusUnauthorized).Send(fiber.Map{"message": "Invalid Credentials"})
 		return
 	}
-	accessToken,err:=utils.CreateJWTToken(map[string]interface{}{"username":user.UserName,"email":user.Email,"id":user.ID,})
+	accessToken, err := utils.CreateJWTToken(map[string]interface{}{"username": user.UserName, "email": user.Email, "id": user.ID})
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -48,4 +48,34 @@ func (a AuthHandler) Login(c *fiber.Ctx)  {
 		return
 	}
 
+}
+func (a AuthHandler) Signup(c *fiber.Ctx) {
+	inputs := new(models.SignupInputs)
+
+	if err := c.BodyParser(inputs); err != nil {
+		c.Status(fiber.StatusBadRequest).Send(err)
+		return
+	}
+	if err := inputs.Validate(); err != nil {
+		c.Status(fiber.StatusBadRequest).Send(err)
+		return
+	}
+
+	query := bson.D{{Key: "email", Value: inputs.Email}}
+	existingUser := new(models.User)
+	err := a.UsersColl.FindOne(c.Fasthttp, query).Decode(existingUser)
+	if err != nil {
+		if err.Error() != "mongo: no documents in result" {
+			log.Fatal(err)
+			return
+
+		}
+	}
+	if existingUser.ID != "" {
+		c.Status(fiber.StatusForbidden).Send(fiber.Map{"message": "User already exists"})
+		return
+	}
+	p := utils.Password{Password: inputs.Password}
+	hashPassword := p.Hash()
+	user := models.User{Email: inputs.Email, Password: hashPassword, UserName: inputs.UserName}
 }
